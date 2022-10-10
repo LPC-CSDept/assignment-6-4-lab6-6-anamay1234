c3c98c25cb6cb45fd1f119078b81e90995c8ba2d
Anamay Singh <anaamaysingh@gmail.com>
2022-10-10 13:32:19 -0700

    6-4 assignement

--- A64.cpp
+++ A64.cpp
@@ -16,6 +16,39 @@ using namespace std;
 // Make a line for Function Prototype for getListprime Here
 //----------
 
+void printPrimeList(int a, int b)
+{
+	int flag;
+	cout << "The primes between " << a << " and " << b << " are - " << endl;
+
+	for(int i=a; i <=b; i++){  
+        if(i == 1 || i == 0)
+           continue;
+        flag =1;
+        for(int j =2; j<= (i/2); ++j){
+            if(i % j == 0){
+                flag = 0;
+                break;
+            }
+        } 
+        if (flag == 1){
+            cout << i << " ";
+        }   
+    }
+}
+
+void getListprime(){
+    printPrimeList(0,100);
+}
+void getListprime(int begin, int end){
+    printPrimeList(begin, end);
+}
+
+void getListprime(int begin){
+    printPrimeList(begin, 100);
+}
+
+
 int main()
 {
 	int begin, end;
